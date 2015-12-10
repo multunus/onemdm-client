3580892b526e361c0805c8854b7891d5e121113d
Leena <leenasn@gmail.com>
2015-12-10 16:56:49 +0530

    Change the script to upload debug APK as demo

--- publish.sh
+++ publish.sh
@@ -2,3 +2,4 @@
 
 cd  app/build/outputs/apk
 aws s3 cp onemdm-v*.apk s3://onemdm/onemdm.apk --grants "read=uri=http://acs.amazonaws.com/groups/global/AllUsers"
+aws s3 cp onemdm-debug-v*.apk s3://onemdm/onemdm-demo.apk --grants "read=uri=http://acs.amazonaws.com/groups/global/AllUsers"
